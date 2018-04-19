8c13fbdaac7fd1853b5869235e503717c653d257
jurmann <joseph@swoopsrch.com>
2018-04-18 22:43:40 -0500

    updated to work with springer.com

--- scraper.py
+++ scraper.py
@@ -1,56 +1,100 @@
 import urllib2
 from bs4 import BeautifulSoup
+import requests
+import sys
+from time import sleep
+from random import randint
+import os
+import csv
 
 # specify the url
 def get_data(urls=None):
 
+    for url in urls:
 
-    # test urls
-    quote_page = "https://link.springer.com/article/10.1007/s00253-017-8124-9"
-    quote_page_2 = "https://link.springer.com/article/10.1007/s00253-005-0263-8"
-    # query the website and return the html to the variable
-    page = urllib2.urlopen(quote_page)
+        # query the website and return the html to the variable
+        page = urllib2.urlopen(url)
 
-    # parse the html using beautiful soup and store in variable `soup`
-    soup = BeautifulSoup(page, "html.parser")
+        try:
+            # parse the html using beautiful soup and store in variable `soup`
+            soup = BeautifulSoup(page, "html.parser", from_encoding="utf-8")
 
-    # get article title
-    title = soup.find("h1", attrs={"class": "ArticleTitle"})
-    article_title = title.text
+            # get article title
+            title = soup.find("h1", attrs={"class": "ArticleTitle"})
+            article_title = title.text
 
-    # get article date
-    date = soup.find("span", attrs={"class": "ArticleCitation_Year"})
-    article_date = date.text[:-2]
+            # get article date
+            date = soup.find("span", attrs={"class": "ArticleCitation_Year"})
+            article_date = date.text[:-2]
 
-    # get list of authors
-    authors_list = []
-    authors = soup.find_all("span", attrs={"class": "authors__name"})
-    for author in authors:
-        authors_list.append(author.text.replace(u'\xa0', u' '))
+            # get list of authors
+            authors_list = []
+            authors = soup.find_all("span", attrs={"class": "authors__name"})
+            for author in authors:
+                authors_list.append(author.text)
+            authors_string = ', '.join(authors_list)
+            print authors_string
 
-    # get the index price
-    contact = soup.find("a", attrs={"class":"gtm-email-author"})
-    contact_author = contact.parent.parent.parent.text[:-12]
-    contact_email = contact.get('title')
+            # get the index price
+            contact = soup.find("a", attrs={"class":"gtm-email-author"})
+            contact_author = contact.parent.parent.parent.text[:-12]
+            contact_email = contact.get('title')
 
-    # get article keywords
-    article_keywords = []
-    keywords = soup.find_all("span", attrs={"class": "Keyword"})
-    for keyword in keywords:
-        article_keywords.append(keyword.text.replace(u'\xa0', u' '))
+            # get article keywords
+            article_keywords = []
+            keywords = soup.find_all("span", attrs={"class": "Keyword"})
+            for keyword in keywords:
+                article_keywords.append(keyword.text)
+            keywords_string = ', '.join(article_keywords)
 
-    print article_title
-    print article_date
-    print authors_list
-    print contact_author
-    print contact_email
-    print article_keywords
+            output = [article_title, article_date, authors_string, contact_author, contact_email, keywords_string, url]
+        except:
+            continue
 
+        output_to_csv(output)
+
+
+def get_urls(url, pages=10):
+
+    urls = []
+
+    n = 1
+    while n <= pages:
+
+        text = "&start="
+        number = str((n - 1) * 10)
+        google_results = str(url + text + number)
+
+        # query the website and return the html to the variable
+        response = requests.get(google_results)
+
+        # parse the html using beautiful soup and store in variable `soup`
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        for item in soup.find_all('h3', attrs={'class' : 'gs_rt'}):
+            urls.append(item.a['href'])
+        n += 1
+
+        # sleeping to not break google's policy
+        sleep(randint(4, 8))
+    get_data(urls)
 
 
 def output_to_csv(data):
-    pass
+    # location where data is output in csv format (this is the location in the container)
+    filename = '~/test_google_scholar_scaper.csv'
+    file_exists = os.path.exists(filename)
+    headers = ["article_title", "article_date", "authors_list", "contact_author", "contact_email", "article_keywords", "url"]
+
+    # outputting data to csv file
+    # NOTE: Excel does not show utf-8 characters
+    with open(filename, 'a') as csv_file:
+        writer = csv.writer(csv_file)
+        if not file_exists:
+            writer.writerow(headers)
+        writer.writerow([unicode(dat).encode("utf-8") for dat in data])
 
 if __name__ == "__main__":
-    # pages = int(sys.argv[1])
-    get_data()
+    URL = sys.argv[1]
+    PAGES = int(sys.argv[2])
+    get_urls(URL, PAGES)
